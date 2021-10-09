8e0dec287320a4d8ecdd413c4c8f753c0e3bbb95
cupssky <cupssky@naver.com>
2021-10-09 16:58:48 +0900

    feat: 콤마 split 가능 여부 체크 기능 추가 - ValidateUtil.java isSplit 메소드 추가

--- src/main/java/racinggame/utils/ValidateUtil.java
+++ src/main/java/racinggame/utils/ValidateUtil.java
@@ -2,7 +2,16 @@ package racinggame.utils;
 
 public class ValidateUtil {
 
+  private static String COMMA = ",";
+
   public static boolean isBlank(String value) {
     return value == null || value.trim().isEmpty();
+  }
+
+  public static boolean isSplit(String value) {
+    if(isBlank(value)) {
+      return false;
+    }
+    return value.split(COMMA).length > 1;
   }
 }
