fa09fafbe558723ddb59590942501bb59e7b3784
cupssky <cupssky@naver.com>
2021-10-11 12:38:57 +0900

    test: 특정 거리 이상 움직인 자동차 판단 기능 테스트 - 현재 자동차가 특정거리 이상 움직였는지 검증 메소드 테스트 - 현재 자동차가 특정거리 이상 움직인차 정보 메소드 테스트

--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -3,12 +3,15 @@ package racinggame.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarTest {
 
     private static final String CAR_TEMP_NAME = "GV60";
+    private static final int TEST_MOVE_NUMBER = 5;
     private Car car;
 
     @BeforeEach
@@ -25,5 +28,33 @@ public class CarTest {
         assertThat(car.getDistance()).isEqualTo(1);
         car.move(2);
         assertThat(car.getDistance()).isEqualTo(1);
+    }
+
+    @DisplayName("목표거리 지나가지 않은 자동차 정보 못가져오는지 기능 테스트 (결과 null)")
+    @ValueSource(ints = 4)
+    @ParameterizedTest
+    void getDistanceOverCarsIsNullTest(int distance) {
+        assertThat(car.getDistanceOverCar(distance)).isNull();
+    }
+
+    @DisplayName("목표거리 지나간 자동차인지 검증 기능 테스트 (움직인 차가 아니라서 결과 false)")
+    @ValueSource(ints = 4)
+    @ParameterizedTest
+    void isDistanceOverCarsTest(int distance) {
+        assertThat(car.isDistanceOverCar(distance)).isFalse();
+    }
+
+    @DisplayName("목표거리 지나간 자동차 정보 가져오는지 기능 테스트")
+    @ValueSource(ints = 4)
+    @ParameterizedTest
+    void getDistanceOverCarsTest(int distance) {
+        getTestMoveCar(6);
+        assertThat(car.getDistanceOverCar(distance)).isNotNull();
+    }
+
+    void getTestMoveCar(int moveCount) {
+        for (int i = 1; i <= moveCount; i++) {
+            car.move(TEST_MOVE_NUMBER);
+        }
     }
 }
