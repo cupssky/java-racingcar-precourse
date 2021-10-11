ebc2b4ade56e5c47ffe161902d68e39bdf45456e
cupssky <cupssky@naver.com>
2021-10-11 12:57:07 +0900

    feat: 입력값 숫자값 검증기능 - ValidateUtil.java 입력값 숫자값확인 메소드

--- src/main/java/racinggame/utils/ValidateUtil.java
+++ src/main/java/racinggame/utils/ValidateUtil.java
@@ -2,16 +2,26 @@ package racinggame.utils;
 
 public class ValidateUtil {
 
-  private static String COMMA = ",";
+    private static String COMMA = ",";
 
-  public static boolean isBlank(String value) {
-    return value == null || value.trim().isEmpty();
-  }
-
-  public static boolean isSplit(String value) {
-    if(isBlank(value)) {
-      return false;
+    public static boolean isBlank(String value) {
+        return value == null || value.trim().isEmpty();
     }
-    return value.split(COMMA).length > 1;
-  }
+
+    public static boolean isSplit(String value) {
+        if(isBlank(value)) {
+            return false;
+        }
+        return value.split(COMMA).length > 1;
+    }
+
+    public static boolean isNumber(String value) {
+        if (isBlank(value)) {
+            return false;
+        }
+        if (!value.matches("[0-9]+")) {
+            return false;
+        }
+        return true;
+    }
 }
