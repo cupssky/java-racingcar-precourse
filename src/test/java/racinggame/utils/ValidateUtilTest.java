d7b546919afcf464e10c25090779d2158cf59cb4
cupssky <cupssky@naver.com>
2021-10-11 12:57:52 +0900

    test: 입력값 숫자값 검증기능 테스트 - ValidateUtil.java 입력값 숫자값확인 테스트

--- src/test/java/racinggame/utils/ValidateUtilTest.java
+++ src/test/java/racinggame/utils/ValidateUtilTest.java
@@ -9,33 +9,48 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidateUtilTest {
 
-  @DisplayName("입력값 공백검증 테스트(공백이면 True)")
-  @NullAndEmptySource
-  @ValueSource(strings = {" ", "\n"})
-  @ParameterizedTest
-  void isBlankTrueTest(String input) {
-    assertThat(ValidateUtil.isBlank(input)).isTrue();
-  }
+    @DisplayName("입력값 공백검증 테스트(공백이면 True)")
+    @NullAndEmptySource
+    @ValueSource(strings = {" ", "\n"})
+    @ParameterizedTest
+    void isBlankTrueTest(String input) {
+        assertThat(ValidateUtil.isBlank(input)).isTrue();
+    }
 
-  @DisplayName("입력값 공백검증 테스트(값이 존재하는 False)")
-  @ValueSource(strings = {"테스트", "cupssky"})
-  @ParameterizedTest
-  void isBlankFalseTest(String input) {
-    assertThat(ValidateUtil.isBlank(input)).isFalse();
-  }
+    @DisplayName("입력값 공백검증 테스트(값이 존재하는 False)")
+    @ValueSource(strings = {"테스트", "cupssky"})
+    @ParameterizedTest
+    void isBlankFalseTest(String input) {
+        assertThat(ValidateUtil.isBlank(input)).isFalse();
+    }
 
-  @DisplayName("입력값 콤마 SPLIT여부 테스트(SPLIT 가능한 경우 true)")
-  @ValueSource(strings = {"테스트,테스트", "테스트,abcd", "1234,테스트"})
-  @ParameterizedTest
-  void isSplitTrueTest(String input) {
-    assertThat(ValidateUtil.isSplit(input)).isTrue();
-  }
+    @DisplayName("입력값 콤마 SPLIT여부 테스트(SPLIT 가능한 경우 true)")
+    @ValueSource(strings = {"테스트,테스트", "테스트,abcd", "1234,테스트"})
+    @ParameterizedTest
+    void isSplitTrueTest(String input) {
+        assertThat(ValidateUtil.isSplit(input)).isTrue();
+    }
 
-  @DisplayName("입력값 콤마 SPLIT여부 테스트(SPLIT 불가능한 경우 false)")
-  @ValueSource(strings = {"테스트", " ", "\n"})
-  @NullAndEmptySource
-  @ParameterizedTest
-  void isSplitTrueFalse(String input) {
-    assertThat(ValidateUtil.isSplit(input)).isFalse();
-  }
+    @DisplayName("입력값 콤마 SPLIT여부 테스트(SPLIT 불가능한 경우 false)")
+    @ValueSource(strings = {"테스트", " ", "\n"})
+    @NullAndEmptySource
+    @ParameterizedTest
+    void isSplitFalseTest(String input) {
+        assertThat(ValidateUtil.isSplit(input)).isFalse();
+    }
+
+    @DisplayName("입력값 숫자값 여부 테스트(숫자값인 경우 true)")
+    @ValueSource(strings = {"1", "55", "12"})
+    @ParameterizedTest
+    void isNumberTrueTest(String input) {
+        assertThat(ValidateUtil.isNumber(input)).isTrue();
+    }
+
+    @DisplayName("입력값 숫자값 여부 테스트(숫자값인 아닌경우 false")
+    @ValueSource(strings = {"김", "abcd", "12김"})
+    @NullAndEmptySource
+    @ParameterizedTest
+    void isNumberFalseTest(String input) {
+        assertThat(ValidateUtil.isNumber(input)).isFalse();
+    }
 }
