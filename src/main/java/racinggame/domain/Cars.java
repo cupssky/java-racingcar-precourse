09c607114c85ccf9a2890eca1b031b7a91187876
cupssky <cupssky@naver.com>
2021-10-12 11:00:06 +0900

    feat: 자동차 일급컬렉션 Cars 추가 - Cars 관련 필요 기능 메소드 추가 - 자동차들 움직이기, 자동차들중 가장먼거리 도달한 차들 가져오기

---
+++ src/main/java/racinggame/domain/Cars.java
@@ -0,0 +1,57 @@
+package racinggame.domain;
+
+import nextstep.utils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static racinggame.view.GameView.printNewLine;
+import static racinggame.view.GameView.printResult;
+
+public class Cars {
+
+    private List<Car> cars;
+
+    public Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void move() {
+        for (Car car : cars) {
+            car.move(Randoms.pickNumberInRange(0, 9));
+        }
+    }
+
+    public void result() {
+        for (Car car : cars) {
+            printResult(car);
+        }
+        printNewLine();
+    }
+
+    public List<Car> getMaxDistanceCars() {
+        List<Car> maxDistanceCars = new ArrayList<>();
+        int max = getMaxDistance();
+
+        for (Car car : cars) {
+            maxDistanceCars = generateMaxDistanceCars(maxDistanceCars, car, max);
+        }
+        return maxDistanceCars;
+    }
+
+    public int getMaxDistance() {
+        int max = 0;
+        for (Car car : cars) {
+            max = Math.max(max, car.getDistance());
+        }
+        return max;
+    }
+
+    private List<Car> generateMaxDistanceCars(List<Car> maxDistanceCars, Car car, int max) {
+        car = car.getMaxDistanceCar(max);
+        if (car != null) {
+            maxDistanceCars.add(car);
+        }
+        return maxDistanceCars;
+    }
+}
