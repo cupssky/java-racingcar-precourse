76b7b5a5c371a378b22680272fa95192a3d01710
cupssky <cupssky@naver.com>
2021-10-12 20:35:35 +0900

    refactor: split 문자열 마지막에 콤마일 경우 예외 수정 - ValidateUtil.java 분기 false 리턴 추가

--- src/main/java/racinggame/utils/ValidateUtil.java
+++ src/main/java/racinggame/utils/ValidateUtil.java
@@ -4,12 +4,22 @@ public class ValidateUtil {
 
     private static String COMMA = ",";
 
+    public static boolean isValidLength(String value, int length) {
+        if (value.length() > length) {
+            return false;
+        }
+        return true;
+    }
+
     public static boolean isBlank(String value) {
         return value == null || value.trim().isEmpty();
     }
 
     public static boolean isSplit(String value) {
-        if(isBlank(value)) {
+        if (isBlank(value)) {
+            return false;
+        }
+        if (value.substring(value.length() - 1).equals(COMMA)) {
             return false;
         }
         return value.split(COMMA).length > 1;
@@ -24,4 +34,5 @@ public class ValidateUtil {
         }
         return true;
     }
+
 }
