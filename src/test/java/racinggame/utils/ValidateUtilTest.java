fabd122772c3b848160273ecdeb4c09b9c4c3298
cupssky <cupssky@naver.com>
2021-10-12 20:36:39 +0900

    refactor: isSplit 메소트 테스트 케이스 추가 - 마지막 문자 콤마 관련 테스트 케이스 추가

--- src/test/java/racinggame/utils/ValidateUtilTest.java
+++ src/test/java/racinggame/utils/ValidateUtilTest.java
@@ -3,6 +3,7 @@ package racinggame.utils;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -32,7 +33,7 @@ public class ValidateUtilTest {
     }
 
     @DisplayName("입력값 콤마 SPLIT여부 테스트(SPLIT 불가능한 경우 false)")
-    @ValueSource(strings = {"테스트", " ", "\n"})
+    @ValueSource(strings = {"테스트", " ", "\n", ",,", "a,"})
     @NullAndEmptySource
     @ParameterizedTest
     void isSplitFalseTest(String input) {
